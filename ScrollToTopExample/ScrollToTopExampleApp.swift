9c9514dc8af7e2fafaf135df00ed4ffe69ac1daf
katafuchix <katafuchix@gmail.com>
2023-01-10 13:06:00 +0900

    Initial Commit

---
+++ ScrollToTopExample/ScrollToTopExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToTopExampleApp.swift
+//  ScrollToTopExample
+//
+//  Created by cano on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToTopExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
